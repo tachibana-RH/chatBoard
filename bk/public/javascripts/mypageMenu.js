a616cbf2f9c2a869fd91337f68eb5b52c1f77f58
tachibana-RH <g1323204rh@gmail.com>
2019-10-04 18:15:51 +0900

    レスポンシブ再修正

--- bk/public/javascripts/mypageMenu.js
+++
@@ -1,7 +0,0 @@
-$(function() {
-    $(document).on('click', '.menu li', function(){
-        if ($(this).attr("value") != '') {
-            window.location.href = $(this).attr("value");
-        }
-    });
-})
